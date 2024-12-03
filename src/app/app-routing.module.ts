9d566f419ebeb46111f11539ed5b59ea2df566f8
MontassarbellahAbdallah <amontasar2@gmail.com>
2024-12-03 16:46:10 +0100

    integration

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AjouterAdminComponent } from './ajouter-admin/ajouter-admin.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'',component:AjouterAdminComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AjouterAdminComponent } from './ajouter-admin/ajouter-admin.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { DashbordComponent } from './dashbord/dashbord.component';
+import { MenuComponent } from './menu/menu.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    AjouterAdminComponent
+    AjouterAdminComponent,
+    HeaderComponent,
+    FooterComponent,
+    DashbordComponent,
+    MenuComponent
   ],
   imports: [
     BrowserModule,
